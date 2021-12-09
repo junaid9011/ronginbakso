8645d2b1499f5417cccc25be3c0176a52698d7e1
xunaid5 <junaid.cou@gmail.com>
2021-12-09 23:26:53 +0600

    done

--- src/Componants/Planning/Planning.js
+++ src/Componants/Planning/Planning.js
@@ -7,10 +7,10 @@ const Planning = () => {
     }, [])
     return (
         <div data-aos="fade-out"  className=" w-100  p-4 text-center ">
-            <h1 className="py-10 text-gray-400 font-semibold hover:text-gray-800 border-b">GOALS</h1>
+            <h1 className="py-10 text-gray-400 font-semibold text-2xl hover:text-gray-800 border-b">GOALS</h1>
             <div  className="sm:flex pt-2 justify-around">
                 <div  data-aos="fade-out" className=" md:w-1/4 ">
-                    <h1 className="font-medium">Help</h1>
+                    <h1 className=" text-2xl font-medium">Help</h1>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit quae asperiores unde, culpa eveniet corporis aspernatur officia corrupti modi consequuntur iste hic consequatur, nihil quidem accusamus magni sunt, eligendi sequi!</p>
                 </div>
                 <div data-aos="fade-out" className="md:w-1/4">
